b40b54ba1efd0a9cc3217cff7bd7e59612bae50b
abcdragon <dragonabc123@naver.com>
2020-06-21 00:36:11 +0900

    Issue #6 #8

--- calc/sum_product.py
+++ calc/sum_product.py
@@ -5,18 +5,20 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	a = d.get('a', [''])[0]
 	b = d.get('b', [''])[0]
-	response_body = html
+	response_body = html.split('\n')
 	try:
-		response_body = response_body.split('\n')
 		a, b = int(a), int(b)
 		response_body[8] += str(a+b)
 		response_body[9] += str(a*b)
-		response_body = '\n'.join(response_body)
 			
 	except:
-		if a == '': response_body[3] = response_body[3][:-8] + ' a is empty.' + '<br><br>'
-		if b == '': response_body[4] = response_body[4][:-8] + ' b is empty.' + '<br><br>'
+		if a == '':
+			response_body[4] = b' a is empty.' 
+		if b == '': 
+			response_body[6] = b' b is empty.'
 
+
+	response_body = '\n'.join(response_body)
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
 		('Content-Length', str(len(response_body)))

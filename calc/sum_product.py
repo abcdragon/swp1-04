63ff2da343d22ca8699887355c7024d61fa89d6c
abcdragon <heybrostop@gmail.com>
2020-06-20 21:29:06 +0900

    Issue #5

--- calc/sum_product.py
+++ calc/sum_product.py
@@ -3,13 +3,19 @@ from template import html
 
 def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
-	a = int(d.get('a', [''])[0])
-	b = int(d.get('b', [''])[0])
+	a = d.get('a', [''])[0]
+	b = d.get('b', [''])[0]
 	response_body = html
-	response_body = response_body.split('\n')
-	response_body[8] += str(a+b)
-	response_body[9] += str(a*b)
-	response_body = '\n'.join(response_body)
+	# a, b 가 숫자가 아닐 경우
+	try:
+		a, b = int(a), int(b)
+		response_body = response_body.split('\n')
+		response_body[8] += str(a+b)
+		response_body[9] += str(a*b)
+		response_body = '\n'.join(response_body)
+	except:
+		pass
+
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
 		('Content-Length', str(len(response_body)))
